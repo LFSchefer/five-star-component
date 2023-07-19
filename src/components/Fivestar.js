d575508031c12b1435a4dc855b5452f28cba27bc
louis frederic schefer <lf.schefer@gmail.com>
2023-07-19 12:27:06 +0200

    preview mouse over

--- src/components/Fivestar.js
+++ src/components/Fivestar.js
@@ -5,10 +5,14 @@ import { faStar } from '@fortawesome/free-solid-svg-icons'
 
 
 const Fivestar = (props) => {
-  const [numberOfStar, setNumberOfStar] = React.useState([false,false,false,false,false])
+  const [numberOfStar, setNumberOfStar] = React.useState([true,false,false,false,false])
+  const [rank, setRank] = React.useState(0)
 
   const handleClick = (index) => {
+    setRank(index)
+  }
 
+  const mouseEnter = (index) => {
     setNumberOfStar(prev => {
       const star = [...prev]
       for (let i = 0; i <= 4; i++) {
@@ -18,8 +22,21 @@ const Fivestar = (props) => {
     })
   }
 
+  const mouseLeave = (index) => {
+    setNumberOfStar(prev => {
+      const star = [...prev]
+      for (let i = 0; i <= 4; i++) {
+        i <= rank ? star[i] = true : star[i] = false
+      }
+      return star
+    })
+  }
+
   const fiveStar = numberOfStar.map((value, index) => {
-    return <div className="star" key={index} onClick={(event) => handleClick(index, event)}>
+    return <div className="star" key={index}
+    onClick={(event) => handleClick(index, event)}
+    onMouseEnter={(event) => mouseEnter(index, event)}
+    onMouseLeave={(event) => mouseLeave(index, event)}>
       {value ?
     <FontAwesomeIcon icon={faStar} style={{color: "#ffdd00",}} size="2x"/> :
     <FontAwesomeIcon icon={faStar} style={{color: "#000000"}} size="2x"/>}
@@ -29,6 +46,8 @@ const Fivestar = (props) => {
   return (
     <>
     <div className="five-star">
+      <p>Rank :</p>
+      {rank + 1}
       {fiveStar}
     </div>
     </>
